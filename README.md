bb3a8a8deb165b76bf08506610ef813bc9346a79
Scott G <scottgarberjr1@gmail.com>
2022-12-29 10:08:25 -0500

    made quick app and compared to vanilla js app

---
+++ README.md
@@ -0,0 +1,8 @@
+# vue3tut-js-counter
+counter webapp made following Laith Academy course
+
+- [Laith Academy course](https://youtu.be/I_xLMmNeLDY)
+- built in vanilla js ot compare same app built using Vue3 *HERE*
+
+## How to Run:
+- *coming soon*
---
+++ counterApp/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import{ref} from "vue";
+
+  const count = ref(0);
+</script>
+
+
+<template>
+  <div class="container">
+    <div class="counterapp">
+    <h4>
+      The current count is...
+    </h4>
+    <h1>{{ count }}</h1>
+    <button @click="count--">-</button>
+    <button @click="count++">+</button>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+  body, .container{
+    margin:0;
+    padding:0;
+  }
+  .container{
+    background-color: aliceblue;
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .counterapp{
+    text-align: center;
+  }
+</style>
